235a4f565fb6153209c16c8af816674c3bdf25e6
guillaume <guillaume.chevalier.contact@gmail.com>
2021-04-14 16:37:43 +0200

    Json response

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Github\ApiRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,7 +15,9 @@ class DefaultController extends AbstractController
      */
     public function index(ApiRequest $apiRequest, string $user, string $repository, $since = 4, $until = 0): Response
     {
-        $apiRequest->getData($user, $repository, $since, $until);
+        $data = $apiRequest->getData($user, $repository, $since, $until);
+
+        return new JsonResponse($data);
         die();
 
 //        $apiRequest->getData($user, $repository);
---
+++ src/Github/JsonResponse.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Github;
+
+class JsonResponse
+{
+    private $elementFormat = [
+        "year" =>  2021,
+        "week" => 1,
+        "count" => 0,
+        "commits" => [],
+    ];
+
+    public function createResponse()
+    {
+
+    }
+}
